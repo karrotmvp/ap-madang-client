6b5ee56645989eb8b075becad4ae2c36954526a9
Changyu-Ryou <ycg02116@kyonggi.ac.kr>
2021-10-06 11:00:42 +0900

    :sparkles: 웹뷰 종료 이벤트 추가
    karrotmarket/mini를 이용한 미니앱 닫기 이벤트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReservationPage from './pages/ReservationPage/ReservationPage';
 import { Navigator, Screen } from 'karrotframe';
+import Mini from '@karrotmarket/mini';
 
-const App = () => (
-  <Navigator
-    onClose={() => {
-      console.log('닫기버튼이 눌렸습니다');
-    }}
-  >
-    <Screen path="/" component={ReservationPage} />
-    <Screen path="/reservation" component={ReservationPage} />
-  </Navigator>
-);
+const App = () => {
+  const mini = new Mini();
+  return (
+    <Navigator
+      onClose={() => {
+        mini.close();
+      }}
+    >
+      <Screen path="/" component={ReservationPage} />
+      <Screen path="/reservation" component={ReservationPage} />
+    </Navigator>
+  );
+};
 
 export default App;
