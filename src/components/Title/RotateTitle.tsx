021ea7f6363d779058b64df4d4983b213148cfec
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-10-07 23:51:31 +0900

    :dizzy: RotateTitle 전환 animation 추가

--- src/components/Title/RotateTitle.tsx
+++ src/components/Title/RotateTitle.tsx
@@ -1,3 +1,4 @@
+import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
 import React, { useEffect, useState } from 'react';
 import { COLOR } from '../../constant/color';
@@ -12,7 +13,7 @@ const RotateTitle = ({ intervalTime = 1500, items }: Props) => {
     return () => clearInterval(interval);
   }, []);
 
-  return <RotateItem>{items[idx]}</RotateItem>;
+  return <RotateItem intervalTime={intervalTime}>{items[idx]}</RotateItem>;
 };
 
 export default RotateTitle;
@@ -22,11 +23,46 @@ interface Props {
   items: string[];
 }
 
-const RotateItem = styled.div`
+type RotateItemProps = {
+  intervalTime: number;
+};
+
+const titleAnimation = keyframes`
+  0% {
+    transform: translateY(-50px);
+    opacity: 0;
+    -webkit-clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 80%);
+    clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 80%);
+  }
+  20% {
+    transform: translateY(0);
+    opacity: 1;
+    -webkit-clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 15%);
+    clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 15%);
+  }
+  80% {
+    transform: translateY(0);
+    opacity: 1;
+    -webkit-clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 15%);
+    clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 15%);
+  }
+  100% {
+    transform: translateY(50px);
+    opacity: 0;
+    -webkit-clip-path: polygon(100% 0, 100% -0%, 0 100%, 0 100%);
+    clip-path: polygon(100% 0, 100% -0%, 0 100%, 0 100%);
+  }
+`;
+
+const RotateItem = styled('div')`
   font-weight: 800;
   font-size: 4.8rem;
   line-height: 5.76rem;
   letter-spacing: -0.08rem;
   padding: 0.7rem 0;
   color: ${COLOR.LIGHT_GREEN};
+  animation-name: ${titleAnimation};
+  animation-timing-function: ease;
+  animation-duration: ${(props: RotateItemProps) => props.intervalTime + 'ms'};
+  animation-iteration-count: infinite;
 `;
