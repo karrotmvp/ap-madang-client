d7c4cf4bb5ecb290071e296907f46bef4c4b9794
Changyu-Ryou <ycg02116@kyonggi.ac.kr>
2021-10-13 18:22:40 +0900

    :sparkles: Recoil 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 
 import { Global } from '@emotion/react';
 import ReactDOM from 'react-dom';
+import { RecoilRoot } from 'recoil';
 
 import App from './App';
 import reset from './style/reset';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Global styles={reset} />
-    <App />
+    <RecoilRoot>
+      <Global styles={reset} />
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root'),
 );
