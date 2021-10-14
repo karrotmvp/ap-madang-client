c18ecf617dcdf56537edec335c7584c39375d05e
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-10-14 21:33:48 +0900

    :sparkles: customAxios 요청 시 Auth header 추가
    customAxios에 Authorization 헤더를 추가 해 요청하도록 변경

--- src/util/request.ts
+++ src/util/request.ts
@@ -1,9 +1,15 @@
 import axios, { AxiosInstance } from 'axios';
+import Cookies from 'universal-cookie';
 
 const baseURL = process.env.API_URL;
 
+const cookies = new Cookies();
+
 const options = {
   withCredentials: true,
+  headers: {
+    Authorization: cookies.get('Authorization') || '',
+  },
 };
 
 const customAxios: AxiosInstance = axios.create({
