de11aa608d4869763165b9e28f5a4a3671778062
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-10-17 23:58:16 +0900

    :recycle: 모임목록 요청 api 관련 수정
    모임목록 요청 api 내부 로직 변경

--- src/api/meeting.ts
+++ src/api/meeting.ts
@@ -1,11 +1,19 @@
+// import Cookies from 'universal-cookie';
+import Cookies from 'universal-cookie';
+
 import { meetingType } from '../store/meeting';
 import customAxios from '../util/request';
 
 export const getMeetings = async () => {
   try {
-    const result: getMeetingsRes = await customAxios.post('/meetings');
+    const result: getMeetingsRes = await customAxios.post(
+      '/meetings',
+      {},
+      { headers: { Authorization: new Cookies().get('Authorization') } },
+    );
     return { success: true, data: result.data };
   } catch (e) {
+    console.log(e);
     return { success: false };
   }
 };
