a2e1bd67ba84c6a43622397428d175bcbee01fbc
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-10-19 21:15:05 +0900

    :sparkles: 알림 제거 API 관련 로직 추가
    모임 알림 제거 API 관련 로직 추가

--- src/api/alarm.ts
+++ src/api/alarm.ts
@@ -12,6 +12,15 @@ export const newAlarm = async (id: string): Promise<newAlarmRes> => {
   }
 };
 
+export const deleteAlarm = async (id: string): Promise<deleteAlarmRes> => {
+  try {
+    await customAxios().delete(`/alarms/meetings/${id}/`);
+    return { success: true };
+  } catch (e) {
+    return { success: false };
+  }
+};
+
 interface newAlarmRes {
   success: boolean;
   data?: {
@@ -26,9 +35,24 @@ interface newAlarmResultType {
   data: { id: number; meeting: number; user: number; created_at: Date };
 }
 
+interface deleteAlarmRes {
+  success: boolean;
+}
+
 interface MeetingDetailDescriptionType {
   text?: string;
   recommend_user: { text: string }[];
   recommend_topic: { text: string }[];
 }
 
+export interface DeleteAlarmType {
+  id: number;
+  title: string;
+  start_time: string;
+  end_time: string;
+  is_live: boolean;
+  alarm_id: number;
+  description: MeetingDetailDescriptionType;
+  meeting_url: string;
+  region: string;
+}
