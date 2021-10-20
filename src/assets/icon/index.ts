85de7a2e829d6bba909113976bda3b0f91c53ca2
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-10-20 16:18:02 +0900

    :bento: 아이콘 에셋 수정
    아이콘 에셋 파일 업데이트

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,31 +1,6 @@
 import ArrowBackAnd from './ArrowBackAnd';
 import ArrowBackIos from './ArrowBackIos';
-import Check from './Check';
-import Clock from './Clock';
-import Close from './Close';
 import Dot from './Dot';
-import ExpandLess from './ExpandLess';
-import ExpandMore from './ExpandMore';
-import ExpandRight from './ExpandRight';
-import MoreHorizIos from './MoreHorizIos';
-import MoreVert from './MoreVert';
-import NotificationEmpty from './NotificationEmpty';
-import NotificationFilled from './NotificationFilled';
 import NotificationsNone from './Notifications_none';
 
-export {
-  ArrowBackAnd,
-  ArrowBackIos,
-  Check,
-  Close,
-  Clock,
-  Dot,
-  ExpandLess,
-  ExpandMore,
-  ExpandRight,
-  MoreHorizIos,
-  MoreVert,
-  NotificationEmpty,
-  NotificationFilled,
-  NotificationsNone,
-};
+export { ArrowBackAnd, ArrowBackIos, Dot, NotificationsNone };
