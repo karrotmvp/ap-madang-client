e6f5e2b959ef41048b850d40291f55e2570a99ad
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-10-22 18:12:37 +0900

    :sparkles: redirect 페이지 구현
    알림톡을 이용한 모임 접속시 들릴 리다이렉트 페이지 구현

---
+++ src/pages/RedirectPage/RedirectPage.tsx
@@ -0,0 +1,129 @@
+import React, { ReactElement, useEffect } from 'react';
+
+import { keyframes } from '@emotion/react';
+import styled from '@emotion/styled';
+import { ScreenHelmet } from '@karrotframe/navigator';
+
+import nav_back from '../../assets/icon/nav_back.svg';
+import nav_close from '../../assets/icon/nav_close.svg';
+import RedirectHouse from '../../assets/icon/RedirectHouse';
+import { COLOR } from '../../constant/color';
+
+const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+  font-size: 20px;
+`;
+const NavCustomBtn = styled.img`
+  margin-left: 1.5rem;
+`;
+const ContentsArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`;
+
+const TextArea = styled.div`
+  color: ${COLOR.LIGHT_GRAY};
+  margin-top: 1.8rem;
+  margin-bottom: 4.8rem;
+`;
+
+const EnterBtn = styled.div`
+  color: ${COLOR.TEXT_WHITE};
+  border-radius: 0.6rem;
+  background-color: ${COLOR.LIGHT_GREEN};
+  padding: 0.9rem 2rem;
+  font-size: 1.4rem;
+  line-height: 2.3rem;
+  text-align: center;
+  letter-spacing: -0.03rem;
+`;
+
+const changeHouseColor = keyframes`
+  from, 0%, to {
+    fill: ${COLOR.LIGHT_GREEN_000}
+  }
+
+  33% {
+    fill: ${COLOR.LIGHT_GREEN}
+  }
+
+  66% {   
+    fill: ${COLOR.SECONDARY};
+  }
+`;
+
+const changeWindowColor = keyframes`
+  from, 0%, to {
+    fill: ${COLOR.TEXT_WHITE}
+  }
+
+  33% {
+    fill: ${COLOR.SECONDARY}
+  }
+
+  66% {   
+    fill: ${COLOR.LIGHT_GREEN}
+  }
+  }
+`;
+
+const RedirectHouseStyle = styled(RedirectHouse)`
+  .house {
+    animation: ${changeHouseColor} 1s ease infinite;
+  }
+  .window {
+    animation: ${changeWindowColor} 1s ease infinite;
+  }
+`;
+
+const BtnWrapper = styled.div`
+  color: ${COLOR.TEXT_GRAY};
+  margin-bottom: 9rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+
+const GoHomeBtn = styled.span`
+  color: ${COLOR.LIGHT_GREEN};
+`;
+
+function RedirectPage(): ReactElement {
+  const params = new URLSearchParams(location.hash);
+
+  useEffect(() => {
+    const hash = location.hash.split('?')[1].split('&');
+    const hashArr = hash.filter(el => el.includes('meeting'));
+    const url = hashArr[0].split('=')[1];
+    window.open('https://' + url, '', '_blank');
+  }, [params]);
+
+  return (
+    <PageWrapper>
+      <ScreenHelmet
+        customCloseButton={<NavCustomBtn src={nav_close} />}
+        customBackButton={<NavCustomBtn src={nav_back} />}
+      />
+      <ContentsArea>
+        <RedirectHouseStyle />
+        <TextArea className="body2">모임에 입장하는 중이에요</TextArea>
+        <EnterBtn>직접 입장하기</EnterBtn>
+      </ContentsArea>
+
+      <BtnWrapper className="body4">
+        모임에 입장할 수 없나요? <GoHomeBtn>홈으로 돌아가기</GoHomeBtn>
+      </BtnWrapper>
+    </PageWrapper>
+  );
+}
+
+export default RedirectPage;
