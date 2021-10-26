8360b4c3e76ccc90b371575101c45366909b9532
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-10-26 16:15:25 +0900

    :recycle: BrowserRouter 사용
    BrowserRouter 사용

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { css } from '@emotion/css';
 import { Navigator, Screen } from '@karrotframe/navigator';
 import { getAnalytics, logEvent } from 'firebase/analytics';
+import { BrowserRouter } from 'react-router-dom';
 
 import Auth from './hoc/Auth';
 import LandingPage from './pages/LandingPage/LandingPage';
@@ -28,23 +29,23 @@ const App: React.FC = () => {
   }, []);
 
   return (
-    <Navigator
-      theme="Android"
-      onClose={() => mini.close()}
-      className={NavigatorStyle}
-    >
-      <Screen path="/" component={Auth(LandingPage)} />
-      <Screen path="/guide" component={ServiceGuidePage} />
-      <Screen path="/meetings/:id" component={Auth(MeetingDetailPage)} />
-      <Screen path="/onboarding" component={Auth(OnBoardingPage)} />
-      <Screen
-        path="/suggestion/meeting"
-        component={Auth(MeetingSuggestionPage)}
-      />
-      <Screen path="/reservation" component={ReservationPage} />
-      <Screen path="/redirect" component={Auth(RedirectPage)} />
-      <Screen path="*" component={NotFoundPage} />
-    </Navigator>
+    <BrowserRouter>
+      <Navigator
+        useCustomRouter
+        theme="Android"
+        onClose={() => mini.close()}
+        className={NavigatorStyle}
+      >
+        <Screen path="/" component={Auth(LandingPage)} />
+        <Screen path="/guide" component={ServiceGuidePage} />
+        <Screen path="/meetings/:id" component={MeetingDetailPage} />
+        <Screen path="/onboarding" component={Auth(OnBoardingPage)} />
+        <Screen path="/suggestion/meeting" component={MeetingSuggestionPage} />
+        <Screen path="/reservation" component={ReservationPage} />
+        <Screen path="/redirect" component={Auth(RedirectPage)} />
+        <Screen path="*" component={NotFoundPage} />
+      </Navigator>
+    </BrowserRouter>
   );
 };
 
