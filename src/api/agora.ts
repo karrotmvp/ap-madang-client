1c8ff73f4bb3d877d8b2480c5863bcf9ddeee341
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-11-16 23:34:39 +0900

    :sparkles: validateMeetingCode API 요청 코드 구현
    앞에서 받은 meetingPage 용 code를 검증하고 모임방 info를 가져올 수 있는 validateMeetingCode API 구현

--- src/api/agora.ts
+++ src/api/agora.ts
@@ -1,5 +1,16 @@
 import customAxios from '../util/request';
 
+export const validateMeetingCode = async (
+  code: string,
+): Promise<validAgoraCodeRes> => {
+  try {
+    const result = await customAxios().get(`/agora/validate-code?code=${code}`);
+    return { success: true, data: result.data };
+  } catch (e) {
+    return { success: false };
+  }
+};
+
 export const getAgoraCode = async (id: string): Promise<getAgoraCodeRes> => {
   try {
     const result = await customAxios().get(`/agora/code?meeting=${id}`);
@@ -8,6 +19,32 @@ export const getAgoraCode = async (id: string): Promise<getAgoraCodeRes> => {
     return { success: false };
   }
 };
+
+export type InfoType = {
+  meeting: {
+    id: number;
+    title: string;
+    channel_name: string;
+    sub_topics: string[];
+    description: {
+      text: string;
+      recommend_user: { text: string }[];
+      recommend_topic: { text: string }[];
+    };
+  };
+  user: {
+    id: number;
+    nickname: string;
+    profile_image_url: string;
+    manner_temperature: number;
+  };
+  agora_token: string;
+};
+
+interface validAgoraCodeRes {
+  success: boolean;
+  data?: InfoType;
+}
 
 interface getAgoraCodeRes {
   success: boolean;
