540287fecab38a012d7b01a6cdb9a324dcb0b47c
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-11-16 23:36:59 +0900

    :sparkles: meetingPage 라우팅 및 Agora 연결
    meetingPage 라우팅 및 Agora 연결

---
+++ src/components/MeetingPage/index.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+
+import { UID } from 'agora-rtc-react';
+
+import { InfoType, validateMeetingCode } from '../../api/agora';
+import MeetingRoom from './MeetingRoom';
+import WaitingRoom from './WaitingRoom';
+
+export type AgoraRTCUsers = User & audioStreamState;
+
+export type User = {
+  id: UID;
+  manner_temperature: number;
+  nickname: string;
+  profile_image_url: string;
+};
+
+type audioStreamState = { audioStreamValue: boolean };
+
+const AgoraMeetingPage = () => {
+  const [inCall, setInCall] = useState(false);
+  const [info, setInfo] = useState<InfoType | undefined>(undefined);
+
+  const fetchMeetingData = async (code: string) => {
+    const result = await validateMeetingCode(code);
+    if (result.success && result.data) setInfo(result.data);
+  };
+
+  useEffect(() => {
+    if (!info) {
+      const urlHashParams = new URLSearchParams(
+        window.location.hash.substr(window.location.hash.indexOf('?')),
+      );
+      const code = urlHashParams.get('meeting_code');
+      if (code) fetchMeetingData(code);
+    } else if (info) {
+      setInCall(true);
+    }
+  }, [info]);
+
+  return inCall && info ? (
+    <MeetingRoom setInCall={setInCall} info={info} />
+  ) : (
+    <WaitingRoom setInCall={setInCall} />
+  );
+};
+
+export default AgoraMeetingPage;
