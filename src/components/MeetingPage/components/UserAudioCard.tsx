9855d213e285457c592634103e4b1ab0c04da02b
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-11-17 19:29:44 +0900

    :lipstick: UserAudioCard 컴포넌트 스타일 수정
    UserAudioCard 컴포넌트 스타일 수정

--- src/components/MeetingPage/components/UserAudioCard.tsx
+++ src/components/MeetingPage/components/UserAudioCard.tsx
@@ -3,6 +3,7 @@ import React, { useRef } from 'react';
 import styled from '@emotion/styled';
 
 import { User } from '..';
+import userMicOff from '../../../assets/icon/agora/user_micOff.svg';
 import { COLOR } from '../../../constant/color';
 import useOnScreenRatio from '../useOnScreenRatio';
 import { OptionType } from './AudioList';
@@ -24,21 +25,30 @@ export default function UserAudioCard({
 }: Props) {
   const ref = useRef<HTMLDivElement>(null);
   const onScreenRatio = useOnScreenRatio(rootRef, ref);
+
   return (
     <CardOuterWrapper options={options} ratio={onScreenRatio}>
       <CardInnerWrapper
-        volumeState={volumeState}
         options={options}
         profileUrl={user.profile_image_url}
         ref={ref}
         ratio={onScreenRatio}
       >
+        <ProfileImg
+          src={user.profile_image_url}
+          options={options}
+          ratio={onScreenRatio}
+          volumeState={volumeState}
+        />
         <InfoArea>
-          <NickNameInfo>{user.nickname}</NickNameInfo>
-          <LocationInfo options={options}>서초4동</LocationInfo>
+          <NickNameInfo>
+            <NickName>{user.nickname}</NickName>
+            {!user.audioStreamValue && <MicIcon src={userMicOff} />}
+          </NickNameInfo>
+          <LocationInfo options={options}>
+            {user.region_name || '서초2동'}
+          </LocationInfo>
         </InfoArea>
-        {/* TODO: 마이크 on/off 처리 */}
-        {user.audioStreamValue ? '말하는 중' : '마이크끔'}
       </CardInnerWrapper>
     </CardOuterWrapper>
   );
@@ -46,23 +56,24 @@ export default function UserAudioCard({
 
 const CardOuterWrapper = styled.div<{ options: OptionType; ratio: number }>`
   width: ${({ options }) => options.size.toFixed(0) + 'px'};
+  height: ${({ options }) => options.size.toFixed(0) + 'px'};
   //TODO: 입체감 요소
-  height: ${({ options, ratio }) =>
+  /* height: ${({ options, ratio }) =>
     options.size * ratio < options.minSize
       ? options.minSize.toFixed(0) + 'px'
-      : (options.size * ratio).toFixed(0) + 'px'};
-  transition-property: width, height;
-  transition-duration: 0.3s, 0.3s;
+      : (options.size * ratio).toFixed(0) + 'px'}; */
+  transition-property: width, height, background-image;
+  transition-duration: 0.3s, 0.3s, 0.3s;
 
   display: flex;
   justify-content: center;
   align-items: center;
   border: ${({ options }) =>
     options.showAreaBorder ? '1px solid black' : 'none'};
+  box-sizing: border-box;
 `;
 
 const CardInnerWrapper = styled.div<{
-  volumeState: number;
   options: OptionType;
   ratio: number;
   profileUrl: string;
@@ -71,13 +82,15 @@ const CardInnerWrapper = styled.div<{
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  background-image: ${({ profileUrl }) => `url(${profileUrl})`};
-  background-size: ${({ options, ratio }) =>
-    options.size * ratio < options.minSize
-      ? options.minSize.toFixed(0) + 'px'
-      : (options.size * ratio).toFixed(0) + 'px'};
 
   position: relative;
+`;
+
+const ProfileImg = styled.img<{
+  options: OptionType;
+  ratio: number;
+  volumeState: number;
+}>`
   width: ${({ options, ratio }) =>
     options.size * ratio < options.minSize
       ? options.minSize.toFixed(0) + 'px'
@@ -86,20 +99,28 @@ const CardInnerWrapper = styled.div<{
     options.size * ratio < options.minSize
       ? options.minSize.toFixed(0) + 'px'
       : (options.size * ratio).toFixed(0) + 'px'};
-
-  transition-property: width, height;
-  transition-duration: 0.3s, 0.3s;
-
   border-radius: 50%;
 
-  /* background: ${({ volumeState }) =>
-    volumeState > 5 ? 'orange' : 'red'}; */
+  position: relative;
+  z-index: 1;
+
+  border: ${({ volumeState }) =>
+    volumeState > 10 ? '2px solid #41ac70;' : 'none'};
+  -webkit-box-shadow: ${({ volumeState }) =>
+    volumeState > 10 ? '0px 0px 30px #41AC70' : 'none'};
+  box-shadow: ${({ volumeState }) =>
+    volumeState > 10 ? '0px 0px 30px #41AC70' : 'none'};
+
+  transition-property: width, height, background-size, box-shadow;
+  transition-duration: 0.1s, 0.1s, 0.1s, 0.1s;
+
+  box-sizing: border-box;
 `;
 
 const InfoArea = styled.div`
   width: 100%;
   position: absolute;
-  bottom: calc(1.6rem * 2 * -1);
+  bottom: calc(1.6rem * 2 * -1 + -6px);
   font-size: 1.3rem;
   line-height: 1.6rem;
   letter-spacing: -0.3px;
@@ -123,12 +144,25 @@ const NickNameInfo = styled.div`
   text-overflow: ellipsis;
 
   color: ${COLOR.TEXT_BLACK};
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 `;
+
+const NickName = styled.div`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const MicIcon = styled.img``;
 
 const LocationInfo = styled.div<{ options: OptionType }>`
   color: ${COLOR.LIGHT_GREEN};
   width: 100%;
-  /* width: ${({ options }) => options.gutter + 'px'}; */
+
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
