6dd539cdae47c380d18868b66b3be64ef3882822
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-11-17 19:24:37 +0900

    :recycle: createAxios jwt 수정
    createAxios jwt 수정

--- src/util/request.ts
+++ src/util/request.ts
@@ -3,7 +3,10 @@ import axios, { AxiosInstance } from 'axios';
 const baseURL: string = process.env.API_URL || '';
 
 const createAxios = (baseURL: string): AxiosInstance => {
-  const jwtToken = window.localStorage.getItem('Authorization');
+  const jwtToken =
+    window.localStorage.getItem('Authorization') ||
+    window.sessionStorage.getItem('Authorization');
+
   const options = {
     withCredentials: process.env.NODE_ENV !== 'development',
     headers: {
