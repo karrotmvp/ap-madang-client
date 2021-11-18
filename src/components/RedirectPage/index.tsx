c457df725c6b544886f739faeacaff17edec05f1
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-11-18 22:32:17 +0900

    :sparkles: RedirectPage 수정
    RedirectPage 수정

--- src/components/RedirectPage/index.tsx
+++ src/components/RedirectPage/index.tsx
@@ -1,61 +1,17 @@
-import React, { ReactElement, useCallback, useEffect, useState } from 'react';
+import React, { ReactElement, useCallback } from 'react';
 
 import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
-import { logEvent } from '@firebase/analytics';
-import { useNavigator } from '@karrotframe/navigator';
-import { useRecoilValue } from 'recoil';
 
-import { analytics } from '../../App';
 import RedirectHouse from '../../assets/icon/RedirectHouse';
 import { COLOR } from '../../constant/color';
 import { REDIRECT } from '../../constant/message';
-import { userInfoAtom } from '../../store/user';
 import CustomScreenHelmet from '../common/CustomScreenHelmet';
 
 function RedirectPage(): ReactElement {
-  const { replace } = useNavigator();
-  const [redirected, setRedirected] = useState(false);
-  const userInfo = useRecoilValue(userInfoAtom);
-
-  const openNewWindow = (meetingUrl: string, pwd: string) => {
-    window.open(
-      'https://' + meetingUrl.replace('%2F', '/') + '?pwd=' + pwd,
-      '',
-      '_blank',
-    );
-  };
-
-  const redirectToMeet = useCallback(() => {
-    const urlSearchParams = new URLSearchParams(location.search);
-    const url = urlSearchParams.get('meeting_url');
-    const pwd = urlSearchParams.get('pwd');
-    if (url && pwd) {
-      openNewWindow(url, pwd);
-    }
-  }, []);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (!redirected) {
-        redirectToMeet();
-        setRedirected(true);
-      }
-    }, 500);
-    return () => clearTimeout(timer);
-  }, [redirectToMeet, redirected]);
-
   const redirectToHome = useCallback(() => {
-    replace('/');
-  }, [replace]);
-
-  useEffect(() => {
-    if (userInfo) {
-      logEvent(analytics, 'redirect_page__show', {
-        nickname: userInfo?.nickname,
-      });
-    }
-  }, [userInfo]);
+    window.open(process.env.KARROT_SCHEME);
+  }, []);
 
   return (
     <PageWrapper>
@@ -63,7 +19,6 @@ function RedirectPage(): ReactElement {
       <ContentsArea>
         <RedirectHouseStyle />
         <TextArea className="body2">{REDIRECT.TITLE}</TextArea>
-        <EnterBtn onClick={redirectToMeet}>{REDIRECT.BTN_TEXT}</EnterBtn>
       </ContentsArea>
 
       <BtnWrapper className="body4">
@@ -96,17 +51,6 @@ const TextArea = styled.div`
   color: ${COLOR.LIGHT_GRAY};
   margin-top: 1.8rem;
   margin-bottom: 4.8rem;
-`;
-
-const EnterBtn = styled.div`
-  color: ${COLOR.TEXT_WHITE};
-  border-radius: 0.6rem;
-  background-color: ${COLOR.LIGHT_GREEN};
-  padding: 0.9rem 2rem;
-  font-size: 1.4rem;
-  line-height: 2.3rem;
-  text-align: center;
-  letter-spacing: -0.03rem;
 `;
 
 const changeHouseColor = keyframes`
