371dd65c62210152ca3e9a5cf4c8e07516091619
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-11-19 18:02:27 +0900

    :lipstick: GlobalStyle 수정
    GlobalStyle 수정

--- src/style/GlobalStyle.tsx
+++ src/style/GlobalStyle.tsx
@@ -39,7 +39,7 @@ const GlobalStyle = () => {
         }
         .title2 {
           font-size: 1.8rem;
-          line-height: 2.8rem;
+          line-height: 2.7rem;
           font-weight: 600;
           letter-spacing: -0.04rem;
         }
@@ -56,15 +56,15 @@ const GlobalStyle = () => {
         }
         .body2 {
           font-size: 1.6rem;
-          line-height: 2.3rem;
+          line-height: 2.4rem;
         }
         .body3 {
           font-size: 1.5rem;
-          line-height: 2.2rem;
+          line-height: 2.3rem;
         }
         .body4 {
           font-size: 1.4rem;
-          line-height: 2.3rem;
+          line-height: 2.1rem;
         }
 
         @font-face {
