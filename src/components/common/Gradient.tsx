1370912fba2461bdb4c2dbf7aa0e501ed65b3ad9
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-11-21 20:11:53 +0900

    :sparkles: Gradient 컴포넌트 추가
    Gradient 컴포넌트 추가

---
+++ src/components/common/Gradient.tsx
@@ -0,0 +1,37 @@
+import React, { ReactElement } from 'react';
+
+import styled from '@emotion/styled';
+import classnames from 'classnames';
+
+interface Props {
+  gradient: string;
+  children: ReactElement;
+  className?: string;
+}
+
+function Gradient({ gradient, children, className }: Props): ReactElement {
+  return (
+    <GradientStyle
+      gradient={gradient}
+      className={classnames('gradient', className)}
+    >
+      {children}
+    </GradientStyle>
+  );
+}
+
+const GradientStyle = styled.div<{ gradient: string }>`
+  &::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    display: inline-block;
+
+    background: ${({ gradient }) => gradient};
+  }
+`;
+
+export default Gradient;
