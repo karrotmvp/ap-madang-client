677fb80200844e9d9ec370b8fe6c54c2c366f0b5
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-11-23 14:24:24 +0900

    :recycle: 모임 목록 요청시 region_id param 추가
    모임 목록 요청시 region_id param 추가

--- src/api/meeting.ts
+++ src/api/meeting.ts
@@ -2,9 +2,11 @@ import { MeetingList, MeetingDetail } from 'meeting';
 
 import customAxios from '../util/request';
 
-export const getMeetings = async () => {
+export const getMeetings = async (region_id: string) => {
   try {
-    const result: getMeetingsRes = await customAxios().get('/meetings/');
+    const result: getMeetingsRes = await customAxios().get(
+      `/meetings?region_id=${region_id}`,
+    );
     return { success: true, data: result.data };
   } catch (e) {
     return { success: false };
