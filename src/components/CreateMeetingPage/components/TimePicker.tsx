cfaf06a71e39ca8320cecad0e6d09d4ba7ee0e4d
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-12-07 01:58:49 +0900

    :sparkles: TimePicker 컴포넌트 구현
    시작시간과 시작시간으로부터 3시간을 30분 간격으로 종료시간을 선택할 수 있는 TimePicker 컴포넌트 구현

--- src/components/CreateMeetingPage/components/TimePicker.tsx
+++ src/components/CreateMeetingPage/components/TimePicker.tsx
@@ -2,19 +2,28 @@ import React, { ReactElement, useCallback, useEffect, useState } from 'react';
 
 import styled from '@emotion/styled';
 import dayjs from 'dayjs';
+
+import nav_close from '../../../assets/icon/nav_close.svg';
 import { COLOR } from '../../../constant/color';
+
+type TimeType = {
+  start_time: string;
+  end_time: string;
+};
 
 interface Props {
   date: string;
-  setTime: React.Dispatch<React.SetStateAction<string>>;
+  time: TimeType;
+  setTime: React.Dispatch<React.SetStateAction<TimeType>>;
+  trySubmit: boolean;
 }
 
-function TimePicker({ date }: Props): ReactElement {
+function TimePicker({ date, time, setTime, trySubmit }: Props): ReactElement {
   const [startList, setStartList] = useState<dayjs.Dayjs[]>([]);
-  const [startState, setStartState] = useState('');
+  // const [startState, setStartState] = useState('');
 
   const [endList, setEndList] = useState<dayjs.Dayjs[]>([]);
-  const [endState, setEndState] = useState('');
+  // const [endState, setEndState] = useState('');
 
   const startListHandler = useCallback(() => {
     const isToday = dayjs().isSame(date, 'day');
@@ -34,33 +43,42 @@ function TimePicker({ date }: Props): ReactElement {
   }, [date]);
 
   const endListHandler = useCallback(() => {
-    const startTime = dayjs(startState);
+    if (!time.start_time) return;
+    const startTime = dayjs(time.start_time);
     for (let i = 1; i <= 6; i++) {
       const time = startTime.add(i * 30, 'minute');
       setEndList(prevState => {
         return [...prevState, time];
       });
     }
-  }, [date, startState]);
+  }, [time.start_time]);
 
   useEffect(() => {
     setStartList([]);
     setEndList([]);
+    setTime({ start_time: '', end_time: '' });
     startListHandler();
-  }, [date]);
+  }, [date, setTime, startListHandler]);
 
   useEffect(() => {
     setEndList([]);
+    setTime(prevTime => {
+      return { ...prevTime, end_time: '' };
+    });
     startList.length !== 0 && endListHandler();
-  }, [startState]);
+  }, [endListHandler, setTime, startList.length]);
 
   return (
     <TimePickerWrapper>
       <SelectorStyle
         className="start_time_selector"
-        onChange={e => setStartState(e.target.value)}
-        selected={startState ? true : false}
+        onChange={e => {
+          // setStartState(e.target.value);
+          setTime({ start_time: e.target.value, end_time: '' });
+        }}
+        selected={time.start_time ? true : false}
         defaultValue={0}
+        trySubmit={trySubmit}
       >
         <DefaultOption value="시작시간" hidden>
           시작시간
@@ -70,7 +88,7 @@ function TimePicker({ date }: Props): ReactElement {
           return (
             <option
               key={idx.toString() + day.toString()}
-              value={day.format('YYYY-MM-DD hh:mm')}
+              value={day.format('YYYY-MM-DD HH:mm:00')}
             >
               {day.format('a hh:mm')}
             </option>
@@ -80,9 +98,15 @@ function TimePicker({ date }: Props): ReactElement {
       <Tilde />
       <SelectorStyle
         className="end_time_selector"
-        onChange={e => setEndState(e.target.value)}
-        selected={endState ? true : false}
+        onChange={e => {
+          // setEndState(e.target.value);
+          setTime(prevTime => {
+            return { ...prevTime, end_time: e.target.value };
+          });
+        }}
+        selected={time.end_time ? true : false}
         defaultValue={0}
+        trySubmit={trySubmit}
       >
         <DefaultOption value="종료시간" hidden>
           종료시간
@@ -91,7 +115,7 @@ function TimePicker({ date }: Props): ReactElement {
           return (
             <option
               key={idx.toString() + day.toString()}
-              value={day.format('YYYY-MM-DD hh:mm')}
+              value={day.format('YYYY-MM-DD HH:mm:00')}
             >
               {day.format('a hh:mm')}
             </option>
@@ -109,11 +133,11 @@ const TimePickerWrapper = styled.div`
   align-items: center;
 `;
 
-const SelectorStyle = styled.select<{ selected: boolean }>`
+const SelectorStyle = styled.select<{ selected: boolean; trySubmit: boolean }>`
   width: 100%;
   height: 4.7rem;
   color: ${({ selected }) => (selected ? COLOR.TEXT_BLACK : COLOR.GREY_500)};
-  border: 1px solid #cbcccd;
+  padding: 0 1.6rem;
   background-color: white;
   box-sizing: border-box;
   border-radius: 0.6rem;
@@ -122,13 +146,28 @@ const SelectorStyle = styled.select<{ selected: boolean }>`
     no-repeat right #ffffff;
   -webkit-appearance: none;
   background-position-x: calc(100% - 20px);
+
+  border: 1px solid
+    ${({ trySubmit, selected }) =>
+      !selected && trySubmit ? '#ff5638' : '#cbcccd'};
 `;
 
 const DefaultOption = styled.option`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
   font-size: 1.5rem;
   line-height: 2.3rem;
   letter-spacing: -0.03rem;
   color: ${COLOR.GREY_500};
+
+  &:after {
+    content: ${nav_close};
+    background-color: red;
+    background-size: 28px 28px;
+    height: 28px;
+    width: 28px;
+  }
 `;
 
 const Tilde = styled.div`
