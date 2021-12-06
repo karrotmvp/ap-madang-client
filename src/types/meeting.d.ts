82d87f404ac682439701591f71fc54121d5e138a
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-12-07 02:01:23 +0900

    :label: MeetingList 타입 수정
    MeetingList 타입 수정

--- src/types/meeting.d.ts
+++ src/types/meeting.d.ts
@@ -10,8 +10,18 @@ declare module 'meeting' {
     user_enter_cnt: number;
     live_status: 'live' | 'tomorrow' | 'upcoming' | 'finish';
     is_video: boolean;
+    host: HostInfo;
+    is_host: boolean;
     description_text: string;
     alarm_num: number;
+  };
+
+  export type HostInfo = {
+    id: number;
+    nickname: string;
+    profile_image_url: string;
+    manner_temperature: null | number;
+    region_name: null | string;
   };
 
   export type MeetingDetailDescription = {
