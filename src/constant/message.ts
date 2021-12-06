e0cb680cabbaff790650c907fe045cc9bd6dfeff
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-12-07 02:02:31 +0900

    :speech_balloon: message 상수 업데이트
    message 상수 업데이트

--- src/constant/message.ts
+++ src/constant/message.ts
@@ -48,8 +48,7 @@ export const COMMON = {
 };
 
 export const CREATE_MEETING = {
-  NAVIGATOR_TITLE_1: '모임 만들기(1/2)',
-  NAVIGATOR_TITLE_2: '모임 만들기(2/2)',
+  NAVIGATOR_TITLE: '모임 만들기',
 };
 
 export const SUGGESTION = {
@@ -75,6 +74,7 @@ export const MEETING_DETAIL = {
   GREEN_BOX_INFO:
     '모든 모임은 줌(Zoom) 화상 회의로 진행돼요. 줌 앱을 다운로드한 후 이용해 주세요. 카메라를 켜지 않아도 괜찮아요!',
   GREEN_BOX_BTN: '줌 사용방법 보러가기',
+  HOST_FOOTER_MESSAGE: '참여자 모집 중',
   MANNER_CARD: {
     TITLE: '즐거운 랜선동네모임을 위해\n함께 지켜주세요!',
     SUB_TITLE: [
