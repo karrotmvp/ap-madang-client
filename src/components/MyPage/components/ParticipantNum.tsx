522052c2c1355f9d666599231f2b4de7082e05f1
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-12-10 18:24:04 +0900

    :sparkles: 마이페이지 참여이웃 표시 컴포넌트 분리
    마이페이지 참여이웃 표시 컴포넌트 분리

---
+++ src/components/MyPage/components/ParticipantNum.tsx
@@ -0,0 +1,38 @@
+import React, { ReactElement } from 'react';
+
+import styled from '@emotion/styled';
+
+import person_fill__grey from '../../../assets/icon/common/person_fill__grey.svg';
+import { COLOR } from '../../../constant/color';
+
+interface Props {
+  userMeetingNum: number;
+}
+
+function ParticipantNum({ userMeetingNum }: Props): ReactElement {
+  return (
+    <ParticipantNumWrapper>
+      <ParticipantIcon src={person_fill__grey} />
+      <Participant>참여 이웃 {userMeetingNum}명</Participant>
+    </ParticipantNumWrapper>
+  );
+}
+
+const ParticipantNumWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 0.4rem;
+`;
+
+const ParticipantIcon = styled.img`
+  margin-right: 0.6rem;
+`;
+
+const Participant = styled.div`
+  font-size: 1.3rem;
+  line-height: 2rem;
+  color: ${COLOR.FONT_BODY_GREY};
+`;
+
+export default ParticipantNum;
