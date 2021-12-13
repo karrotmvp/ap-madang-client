c6d2fc9310cd0d928ae2c26f7c445416af3e568d
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-12-13 21:20:19 +0900

    :recycle: CustomScreenHelmet 수정
    CustomScreenHelmet 수정

--- src/components/common/CustomScreenHelmet.tsx
+++ src/components/common/CustomScreenHelmet.tsx
@@ -1,16 +1,19 @@
-import React, { ReactElement, useCallback, useMemo } from 'react';
+import React, { useCallback } from 'react';
 
 import styled from '@emotion/styled';
-import { useCurrentScreen, useNavigator } from '@karrotframe/navigator';
+import {
+  ScreenHelmet,
+  useCurrentScreen,
+  useNavigator,
+} from '@karrotframe/navigator';
 
 import nav_back from '../../assets/icon/common/nav_back.svg';
 import nav_close from '../../assets/icon/common/nav_close.svg';
-import { COLOR } from '../../constant/color';
-import mini from '../../util/mini';
 
 type Props = {
   appendRight?: React.ReactNode;
   appendMiddle?: React.ReactNode;
+  appendLeft?: React.ReactNode;
   customCloseButton?: React.ReactNode;
   customBackButton?: React.ReactNode;
   onCustomCloseButton?: () => void;
@@ -24,7 +27,8 @@ function CustomScreenHelmet({
   onCustomBackButton,
   appendRight,
   appendMiddle,
-}: Props): ReactElement {
+  appendLeft,
+}: Props) {
   const { isRoot } = useCurrentScreen();
   const { pop, replace } = useNavigator();
 
@@ -40,82 +44,37 @@ function CustomScreenHelmet({
     }
   }, [isRoot, pop, replace]);
 
-  const backButton = useMemo(() => {
-    return (
-      customBackButton || (
-        <NavCustomBtn
-          src={nav_back}
-          onClick={
-            onCustomBackButton ? onCustomBackButton : () => popCreatedMeeting()
-          }
-        />
-      )
-    );
-  }, [customBackButton, onCustomBackButton, popCreatedMeeting]);
-
-  const closeButton = useMemo(() => {
-    return (
-      customCloseButton || (
-        <NavCustomBtn
-          src={nav_close}
-          onClick={
-            onCustomCloseButton ? onCustomCloseButton : () => mini.close()
-          }
-        />
-      )
-    );
-  }, [customCloseButton, onCustomCloseButton]);
-
-  const leftButton = useMemo(() => {
-    return isRoot ? closeButton : backButton;
-  }, [backButton, closeButton, isRoot]);
-
   return (
-    <ScreenHelmet>
-      <AppendLeft>{leftButton}</AppendLeft>
-      <AppendMiddle>{appendMiddle}</AppendMiddle>
-      <AppendRight>{appendRight && appendRight}</AppendRight>
-    </ScreenHelmet>
+    <ScreenHelmet
+      customCloseButton={
+        customCloseButton || (
+          <NavCustomBtn src={nav_close} onClick={onCustomCloseButton} />
+        )
+      }
+      customBackButton={
+        customBackButton || (
+          <NavCustomBtn
+            src={nav_back}
+            onClick={onCustomBackButton || popCreatedMeeting}
+          />
+        )
+      }
+      title={appendMiddle && <Middle>{appendMiddle}</Middle>}
+      appendLeft={appendLeft && appendLeft}
+      appendRight={appendRight && appendRight}
+    />
   );
 }
 
-const ScreenHelmet = styled.div`
-  position: sticky;
-  top: 0;
-  background: white;
-  width: 100%;
-  height: 5.6rem;
-  min-height: 5.6rem;
-  padding: 0 1.6rem;
-  border-bottom: 1px solid ${COLOR.NAV_BORDER_GREY};
-  box-sizing: border-box;
+const NavCustomBtn = styled.img`
+  margin-left: 1.6rem;
+`;
+
+const Middle = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
   align-items: center;
-  z-index: 10000;
-`;
-
-const NavCustomBtn = styled.img``;
-
-const AppendLeft = styled.div`
-  flex: 1;
-`;
-
-const AppendMiddle = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: row;
   justify-content: center;
-  align-items: center;
-`;
-
-const AppendRight = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  align-items: center;
 `;
 
 export default CustomScreenHelmet;
