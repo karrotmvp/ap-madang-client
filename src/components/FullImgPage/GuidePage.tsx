894355ad2a15ebfde57f4b790e80e4789f820a16
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-12-13 21:21:13 +0900

    :chart_with_upwards_trend: 서비스 가이드 페이지 사용자 추적 이벤트 추가
    서비스 가이드 페이지 사용자 추적 이벤트 추가

--- src/components/FullImgPage/GuidePage.tsx
+++ src/components/FullImgPage/GuidePage.tsx
@@ -1,9 +1,15 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect } from 'react';
 
+import { logEvent } from '@firebase/analytics';
+
+import { analytics } from '../../App';
 import service_guide from '../../assets/image/service_guide.png';
 import FullImgPage from './FullImgPage';
 
 function GuidePage(): ReactElement {
+  useEffect(() => {
+    logEvent(analytics, 'home_banner_service__show');
+  }, []);
   return <FullImgPage imgSource={service_guide} />;
 }
 
