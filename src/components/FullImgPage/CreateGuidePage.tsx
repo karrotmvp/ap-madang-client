648b4ca48625d80d8adf36b3c6af820d1463f889
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2021-12-13 21:21:05 +0900

    :chart_with_upwards_trend: 모임생성 가이드 페이지 사용자 추적 이벤트 추가

--- src/components/FullImgPage/CreateGuidePage.tsx
+++ src/components/FullImgPage/CreateGuidePage.tsx
@@ -1,10 +1,16 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect } from 'react';
 
+import { logEvent } from '@firebase/analytics';
+
+import { analytics } from '../../App';
 import create_meeting_guide from '../../assets/image/create_meeting_guide.png';
 import CreateFooter from './components/CreateFooter';
 import FullImgPage from './FullImgPage';
 
 function CreateGuidePage(): ReactElement {
+  useEffect(() => {
+    logEvent(analytics, 'home_banner_create_meeting__show');
+  }, []);
   return (
     <FullImgPage imgSource={create_meeting_guide} footer={<CreateFooter />} />
   );
