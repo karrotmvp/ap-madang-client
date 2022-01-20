89602939b174c338e0b8c0464b67d54bc78efc1c
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2022-01-21 00:26:51 +0900

    :sparkles: 음성모임 생성 안내 페이지 구현
    음성모임 생성 안내 페이지 구현

--- src/components/LinkGeneratorPage/index.tsx
+++ src/components/LinkGeneratorPage/index.tsx
@@ -1,16 +1,22 @@
 import React, { ReactElement, useState } from 'react';
 
 import styled from '@emotion/styled';
+import { useRecoilValue } from 'recoil';
 
 import { generateShortLink } from '../../api/meeting';
+import speaker from '../../assets/icon/linkGenerator/speaker.svg';
 import useMini from '../../hook/useMini';
+import { userInfoAtom } from '../../store/user';
+import { COLOR } from '../../style/color';
 import CustomScreenHelmet from '../common/CustomScreenHelmet';
 import LinkBottomSheet from './components/LinkBottomSheet';
+import PrimaryButton from './components/PrimaryButton';
 
 function LinkGeneratorPage(): ReactElement {
   const [openLinkBottomSheet, setOpenLinkBottomSheet] = useState(false);
   const [url, setUrl] = useState('');
   const { loginWithMini } = useMini();
+  const userInfo = useRecoilValue(userInfoAtom);
 
   const onClickGenerateLink = async () => {
     const result = await generateShortLink();
@@ -23,7 +29,7 @@ function LinkGeneratorPage(): ReactElement {
   };
 
   return (
-    <div>
+    <Container>
       <CustomScreenHelmet />
       {openLinkBottomSheet && (
         <LinkBottomSheet
@@ -32,19 +38,85 @@ function LinkGeneratorPage(): ReactElement {
           url={url}
         />
       )}
-      <div onClick={() => setOpenLinkBottomSheet(true)}>LinkGeneratorPage</div>
-      <ButtonStyle onClick={() => loginWithMini(onClickGenerateLink)}>
-        버튼
-      </ButtonStyle>
-
-      <ButtonStyle onClick={onClickGenerateLink}>버튼2</ButtonStyle>
-    </div>
+      <Wrapper>
+        <TopBanner>
+          <SpeakerImage src={speaker} />
+          {userInfo?.nickname && (
+            <UserNameText>{userInfo.nickname}님의</UserNameText>
+          )}
+          <Title>
+            <OrangeTitle>음성 모임방</OrangeTitle>을
+            <br />
+            만들어드려요
+          </Title>
+        </TopBanner>
+      </Wrapper>
+      <FloatButtonWrapper>
+        <PrimaryButton
+          onClick={() => loginWithMini(onClickGenerateLink)}
+          text="모임생성하기"
+        />
+      </FloatButtonWrapper>
+    </Container>
   );
 }
 
-const ButtonStyle = styled.div`
-  color: ${({ theme }) => theme.colors.$button.primary};
-  font-size: 3rem;
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  white-space: pre-line;
+  box-sizing: border-box;
+`;
+
+const Wrapper = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  overflow-y: scroll;
+`;
+
+const TopBanner = styled.div`
+  padding: 6rem 0;
+  background: #fcf5ee;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const SpeakerImage = styled.img``;
+
+const UserNameText = styled.div`
+  margin-top: 2rem;
+  font-size: 2.6rem;
+  line-height: 125%;
+  text-align: center;
+  letter-spacing: -0.02rem;
+  color: #4d5159;
+`;
+
+const Title = styled.span`
+  margin-top: 0.4rem;
+  font-size: 4rem;
+  line-height: 120%;
+  text-align: center;
+  letter-spacing: -0.02rem;
+`;
+
+const OrangeTitle = styled(Title)`
+  color: #fe8745;
+`;
+
+const FloatButtonWrapper = styled.div`
+  max-height: 7rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem 1.6rem;
+  border-top: 1px solid ${COLOR.NAVBAR_TOP_BORDER};
 `;
 
 export default LinkGeneratorPage;
