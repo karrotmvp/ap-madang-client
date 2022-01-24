bd8efc03655595ec77b8ffc1fe5ec235951b118a
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2022-01-24 23:32:01 +0900

    :chart_with_upwards_trend: 브릿지 페이지 이벤트 트래킹

--- src/components/RedirectPage/index.tsx
+++ src/components/RedirectPage/index.tsx
@@ -1,138 +1,94 @@
-import React, { ReactElement, useCallback } from 'react';
+import React, { ReactElement, useEffect } from 'react';
 
-import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
+import { logEvent } from '@firebase/analytics';
+import { useRecoilValue } from 'recoil';
 
-import RedirectHouse from '../../assets/icon/RedirectHouse';
-import { REDIRECT } from '../../constant/message';
-import { COLOR } from '../../style/color';
+import { analytics } from '../../App';
+import orange_house from '../../assets/icon/common/orange_house.svg';
+import { userInfoAtom } from '../../store/user';
 import CustomScreenHelmet from '../common/CustomScreenHelmet';
 
 function RedirectPage(): ReactElement {
-  const redirectToHome = useCallback(() => {
+  const userInfo = useRecoilValue(userInfoAtom);
+  const goBackHandler = () => {
     window.open(process.env.KARROT_SCHEME);
+    window.close();
+  };
+
+  useEffect(() => {
+    loginWithMini();
   }, []);
+
+  useEffect(() => {
+    if (userInfo) {
+      logEvent(analytics, 'meeting_bridge_page__show', {
+        userNickname: userInfo?.nickname,
+        userRegion: userInfo?.region,
+      });
+    }
+  }, [userInfo]);
 
   return (
     <PageWrapper>
-      <CustomScreenHelmet />
-      <ContentsWrapper>
-        <ContentsArea>
-          <RedirectHouseStyle />
-          <TextArea className="body2">{REDIRECT.TITLE}</TextArea>
-        </ContentsArea>
+      <CustomScreenHelmet
+        onCustomBackButton={goBackHandler}
+        onCustomCloseButton={goBackHandler}
+      />
 
-        <BtnWrapper className="body4">
-          {REDIRECT.CANT_JOIN}
-          <GoHomeBtn onClick={redirectToHome}>{REDIRECT.GO_HOME}</GoHomeBtn>
-        </BtnWrapper>
+      <ContentsWrapper className="join WaitingRoom">
+        <Image src={orange_house} />
+        <Title>모임에 들어가는 중이에요</Title>
+        <JoinButton onClick={goBackHandler}>당근마켓으로 돌아가기</JoinButton>
       </ContentsWrapper>
     </PageWrapper>
   );
 }
 
 const PageWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
   width: 100%;
-  font-size: 20px;
+  height: 100%;
 `;
 
 const ContentsWrapper = styled.div`
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
   flex: 1;
 `;
 
-const ContentsArea = styled.div`
+const Image = styled.img`
+  margin-bottom: 2.2rem;
+`;
+
+const Title = styled.div`
+  font-size: 1.6rem;
+  line-height: 2.2rem;
+
+  text-align: center;
+  letter-spacing: -0.04rem;
+
+  color: #5c5c5c;
+  margin-bottom: 2.4rem;
+`;
+
+const JoinButton = styled.div`
+  padding: 0.9rem 1.6rem;
+  background: ${({ theme }) => theme.colors.$button.primary};
+  border-radius: 0.5rem;
+
+  font-size: 1.5rem;
+  line-height: 2.2rem;
+  color: white;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
-  flex: 1;
-`;
-
-const TextArea = styled.div`
-  color: ${COLOR.LIGHT_GREY};
-  margin-top: 1.8rem;
-  margin-bottom: 4.8rem;
-`;
-
-const changeHouseColor = keyframes`
-  0% {
-    fill: ${COLOR.LIGHT_GREEN_000}
-  }
-
-  32% {
-    fill: ${COLOR.LIGHT_GREEN_000}
-  }
-
-  33% {
-    fill: ${COLOR.LIGHT_GREEN}
-  }
-  65% {
-    fill: ${COLOR.LIGHT_GREEN}
-  }
-
-  66% {   
-    fill: ${COLOR.SECONDARY};
-  }
-  100% {   
-    fill: ${COLOR.SECONDARY};
-  }
-`;
-
-const changeWindowColor = keyframes`
-  0%{
-    fill: ${COLOR.TEXT_WHITE}
-  }
-  32%{
-    fill: ${COLOR.TEXT_WHITE}
-  }
-
-  33% {
-    fill: ${COLOR.SECONDARY}
-  }
-  65% {
-    fill: ${COLOR.SECONDARY}
-  }
-
-  66% {   
-    fill: ${COLOR.LIGHT_GREEN}
-  }
-  100% {   
-    fill: ${COLOR.LIGHT_GREEN}
-  }
-  
-`;
-
-const RedirectHouseStyle = styled(RedirectHouse)`
-  .house {
-    animation: ${changeHouseColor} 2s ease infinite;
-  }
-  .window {
-    animation: ${changeWindowColor} 2s ease infinite;
-  }
-`;
-
-const BtnWrapper = styled.div`
-  color: ${COLOR.TEXT_GREY};
-  margin-bottom: 9rem;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  flex-wrap: wrap;
-`;
-
-const GoHomeBtn = styled.div`
-  font-weight: 500;
-  margin-left: 0.5rem;
-  color: ${COLOR.LIGHT_GREEN};
 `;
 
 export default RedirectPage;
+function loginWithMini() {
+  throw new Error('Function not implemented.');
+}
