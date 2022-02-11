71b1bd76132042dd150c1313b8608c1290232831
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2022-02-11 10:27:06 +0900

    :wrench: firebase auth/store config

--- src/util/firebase.ts
+++ src/util/firebase.ts
@@ -1,4 +1,6 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.FA_KEY,
@@ -11,3 +13,6 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
