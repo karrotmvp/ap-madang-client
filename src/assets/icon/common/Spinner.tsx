3a77a4b7b7a1ddbca39784585815a5cffcaf00c7
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2022-02-15 12:34:15 +0900

    :bug: spinner fix

--- src/assets/icon/common/Spinner.tsx
+++ src/assets/icon/common/Spinner.tsx
@@ -19,14 +19,14 @@ function Spinner({ subColor, mainColor, className }: Props): ReactElement {
       <path
         d="M19.9999 37.4433C29.6335 37.4433 37.4432 29.6337 37.4432 20C37.4432 10.3664 29.6335 2.55676 19.9999 2.55676C10.3663 2.55676 2.55664 10.3664 2.55664 20C2.55664 29.6337 10.3663 37.4433 19.9999 37.4433Z"
         stroke={subColor ?? '#D9EEE2'}
-        stroke-width="5"
-        stroke-miterlimit="10"
+        strokeWidth="5"
+        strokeMiterlimit="10"
       />
       <path
         d="M20 2.55676C24.6262 2.55676 29.063 4.39451 32.3342 7.66576C34.853 10.1845 36.5219 13.3943 37.1564 16.8495"
         stroke={mainColor ?? '#41AC70'}
-        stroke-width="5"
-        stroke-miterlimit="10"
+        strokeWidth="5"
+        strokeMiterlimit="10"
       />
     </svg>
   );
