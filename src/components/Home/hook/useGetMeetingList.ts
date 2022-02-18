3cf72cf0835df928beacc1a0844d72f872cec453
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2022-02-19 04:10:39 +0900

    :recycle: useGetMeetingList directory 이동

--- src/components/Home/hook/useGetMeetingList.ts
+++ src/components/Home/hook/useGetMeetingList.ts
@@ -1,29 +1,39 @@
 import { useCallback, useEffect, useState } from 'react';
 
 import { getMeetings } from '@api/v2/meeting';
-import { MeetingList as MeetingListType } from 'meeting-v2';
+import { useQueryParams } from '@karrotframe/navigator';
+import { detailMeetingIdAtom, meetingsAtom } from '@store/meeting';
+import { getRegionId } from '@util/utils';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
 type fetchingStateType = 'LOADING' | 'SUCCESS' | 'ERROR';
+type queryParams = {
+  [key: string]: string;
+};
 
-const useGetMeetingList = (regionId: string) => {
-  const [meetings, setMeetings] = useState<MeetingListType[]>([]);
-
+const useGetMeetingList = () => {
+  const [meetings, setMeetings] = useRecoilState(meetingsAtom);
   const [fetchingState, setFetchingState] =
     useState<fetchingStateType>('LOADING');
+  const queryParams: Partial<queryParams> = useQueryParams();
+  const detailMeetingId = useRecoilValue(detailMeetingIdAtom);
+
+  const regionId = getRegionId(window.location.search);
 
   const meetingListHandler = useCallback(async () => {
     if (!regionId) return;
+
     const result = await getMeetings(regionId);
     if (result.success && result.data) {
       setMeetings(result.data);
       setFetchingState('SUCCESS');
     } else setFetchingState('ERROR');
-  }, [regionId]);
+  }, [regionId, setMeetings]);
 
   useEffect(() => {
     setFetchingState('LOADING');
     meetingListHandler();
-  }, [meetingListHandler]);
+  }, [meetingListHandler, queryParams, detailMeetingId]);
 
   return { meetings, fetchingState };
 };
