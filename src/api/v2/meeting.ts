35aff68a5abd1bb97b13ae0311be8f8ba2345318
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2022-02-22 02:21:04 +0900

    :sparkles: 모임 생성 API 요청부 구현

--- src/api/v2/meeting.ts
+++ src/api/v2/meeting.ts
@@ -1,3 +1,4 @@
+import { FormValues } from '@components/CreatePage';
 import { MeetingList } from 'meeting-v2';
 
 import landongmoAxios from '../../util/request';
@@ -6,6 +7,26 @@ export const getMeetings = async (region_id: string) => {
   try {
     const result: getMeetingsRes = await landongmoAxios().get(
       `/meetings/?region_id=${region_id}/`,
+    );
+    return { success: true, data: result.data };
+  } catch (e) {
+    return { success: false };
+  }
+};
+
+export const createMeetings = async ({
+  title,
+  type,
+  description,
+}: FormValues) => {
+  try {
+    const result: createMeetingsRes = await landongmoAxios().post(
+      `/meetings/`,
+      {
+        title,
+        is_video: type === 'video',
+        description,
+      },
     );
     return { success: true, data: result.data };
   } catch (e) {
@@ -25,4 +46,9 @@ export const closeMeeting = async (meetingId: string) => {
 interface getMeetingsRes {
   success: boolean;
   data?: MeetingList[];
+}
+
+interface createMeetingsRes {
+  success: boolean;
+  data?: { id: number };
 }
